af0ba3b6e72f334f3b81d32dfa2c23af2290bee8
milenabeicker <milenakessia87@gmail.com>
2024-07-15 15:21:00 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Milena Beicker on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
